ac8106c3264e524927eeb37e3b5570f3c345a38e
fazliddinKhayrullaev11 <fazliddinkhayrullaev9@gmail.com>
2023-02-20 16:56:16 +0500

    Data Types

---
+++ JavaScript/Lesson-1/Data types/index.js
@@ -0,0 +1,44 @@
+
+
+
+arr=[1,2,3,4,5,6,7]
+
+let myfunf=(n)=>{
+
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i]!==n){
+            console.log(arr[i]);
+        }
+        
+    }
+
+
+
+
+}
+
+myfunf(4);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Data Types
+
+// Primitive
+// Non-primitive
+
+
+// ctrl+ ` =new terminal
