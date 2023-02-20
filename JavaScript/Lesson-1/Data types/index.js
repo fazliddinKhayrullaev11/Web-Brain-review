9e3499b1abfdb44f419b4f4bcd059af3adecfe35
fazliddinKhayrullaev11 <fazliddinkhayrullaev9@gmail.com>
2023-02-20 17:08:31 +0500

    Clean Code

--- JavaScript/Lesson-1/Data types/index.js
+++ JavaScript/Lesson-1/Data types/index.js
@@ -1,26 +1,18 @@
 
-
-
 arr=[1,2,3,4,5,6,7]
-
 let myfunf=(n)=>{
+for (let i = 0; i < arr.length; i++) {
+if(arr[i]===n){
+  console.log('This number is here');
+   }
+     else{
 
-    for (let i = 0; i < arr.length; i++) {
-        if(arr[i]===n){
-            console.log('This number is here');
-        }
-         else{
-
-            console.log("This number is not on the list");
-        }
+       console.log("This number is not on the list");
+          }
         
-    }
-
-
-
-
-}
-myfunf(8);
+             }
+                }
+                 myfunf(8);
 
 
 
