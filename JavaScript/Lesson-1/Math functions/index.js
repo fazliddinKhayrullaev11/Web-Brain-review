56044cb616d12ddbabc4fa1480f6496e8ec8cab5
fazliddinKhayrullaev11 <fazliddinkhayrullaev9@gmail.com>
2023-02-20 18:14:32 +0500

    Operators

--- JavaScript/Lesson-1/Math functions/index.js
+++ JavaScript/Lesson-1/Math functions/index.js
@@ -13,16 +13,18 @@
 // console.log(name);
 // console.log(p);
 
-var number=199;
-var number=200;
-let name='Fazliddin';
-const p=3.14;
-const pi=3.15;
-console.log(number);
-console.log(name);
-console.log(p);
 
 
+
+// var number=199;
+// var number=200;
+// let name='Fazliddin';
+// name="me"
+// const p=3.14;
+// const pi=3.15;
+// console.log(number);
+// console.log(name);
+// console.log(p);
 
 
 
@@ -32,6 +34,32 @@ console.log(p);
 
 
 // Variables=>ozgaruvhcilar
-// var
-// let
-// const
+// var=>ES6 gacha ishlatilgan , qiymatini ozgartirsa boladi va usha nom bilan yangi ozgaruvchi ochsa ham boladi;
+// let=>ES6 da qoshilgan , qiymatini ozgartirsa boladi , lekin ush nom bilan yangi ozgaruvchi ochib bolmaydi;
+// const=>ES6 da qoshilgan , qiymatini ozgartirib ham , usha nom bilan yangi ozgaruvchi ham ochib bolnmaydi;
+
+
+// Math operators
+
+
+
+// **(1/2)=finding the root
+// console.log(Math.round(64**(1/3))); returns 4
+// console.log(64**(1/2)); returns 8
+
+
+
+// two stars= power
+// console.log(2**4); Returns 16
+
+// percent sign =>remains
+// console.log(11%8);
+
+// forward slash=> division
+// console.log(10/5); returns 2
+
+
+// console.log(10+10);  //returns 20
+// console.log(64**(1/2)); returns 8
+
+
