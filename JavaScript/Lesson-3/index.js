02575236d9b81b3e69084b15523a99427259ea39
fazliddinKhayrullaev11 <fazliddinkhayrullaev9@gmail.com>
2023-02-23 11:06:55 +0500

    String length and index

---
+++ JavaScript/Lesson-3/index.js
@@ -0,0 +1,21 @@
+
+
+// String
+
+
+
+// index and length
+
+var string= 'Fazliddin';
+console.log(string);
+var string= `Fazliddin`;
+console.log(string);
+
+var string= "19252345L";
+console.log(string);
+console.log(string);
+console.log(  +string);
+
+console.log(string[3]);
+console.log( string[string.length-1]);
+
