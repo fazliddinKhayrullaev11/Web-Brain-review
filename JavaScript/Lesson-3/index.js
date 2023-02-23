181e5d9eccd5ef4ab84cd64fbde98a8480bee14e
fazliddinKhayrullaev11 <fazliddinkhayrullaev9@gmail.com>
2023-02-23 11:12:32 +0500

    Pre==backtick ``

--- JavaScript/Lesson-3/index.js
+++ JavaScript/Lesson-3/index.js
@@ -10,12 +10,27 @@ var string= 'Fazliddin';
 console.log(string);
 var string= `Fazliddin`;
 console.log(string);
+console.log(string[string.length-1]);
 
 var string= "19252345L";
-console.log(string);
-console.log(string);
-console.log(  +string);
+console.log(string); //19252345
+console.log(string);//19252345
+console.log(+string); //NaN because of L
 
-console.log(string[3]);
-console.log( string[string.length-1]);
+
+console.log(string[3]);// 5 (0=1, 1=9,2=2,3=5)
+console.log( string[string.length-1]);   //the last letter
+
+
+
+
+var str =`
+
+I will fuck fou!!
+
+
+
+`;
+console.log(str);
+console.log(str.length);
 
