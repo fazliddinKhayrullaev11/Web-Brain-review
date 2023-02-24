7a3deee41411b39c0291fea097d8fc7fec17f781
fazliddinKhayrullaev11 <fazliddinkhayrullaev9@gmail.com>
2023-02-24 15:48:21 +0500

    padStart(), padEnd()

--- JavaScript/Lesson-4/review/index.js
+++ JavaScript/Lesson-4/review/index.js
@@ -2,9 +2,16 @@
 
 
 var mname='Fazliddin';
+var sname='Khayrullaev';
 
 
 console.log(mname.slice(0,5));  //Fazli
 
 console.log(mname.indexOf('i')); //4
 console.log(mname.lastIndexOf('i')); //7
+
+console.log(mname.repeat(10));
+console.log(sname.concat(mname)); //KhayrullaevFazliddin
+console.log(mname.padStart(12,'u'));//uuuFazliddin
+console.log(mname.padEnd(12,'u')); //Fazliddinuuu
+
