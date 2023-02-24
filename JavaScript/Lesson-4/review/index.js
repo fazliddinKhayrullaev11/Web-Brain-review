502d7fbdc0c1ec2355166fd2b4c8d35d4b125eee
fazliddinKhayrullaev11 <fazliddinkhayrullaev9@gmail.com>
2023-02-24 15:53:14 +0500

    startWith(), endsWith(), includes()

--- JavaScript/Lesson-4/review/index.js
+++ JavaScript/Lesson-4/review/index.js
@@ -1,6 +1,8 @@
 
 
 
+
+
 var mname='Fazliddin';
 var sname='Khayrullaev';
 
@@ -14,4 +16,11 @@ console.log(mname.repeat(10));
 console.log(sname.concat(mname)); //KhayrullaevFazliddin
 console.log(mname.padStart(12,'u'));//uuuFazliddin
 console.log(mname.padEnd(12,'u')); //Fazliddinuuu
+
+console.log(mname.includes('Faz')); //true
+console.log(mname.startsWith('F')); //true
+console.log(mname.startsWith('a')); //false
+
+
+
 
