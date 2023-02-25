d6e058545b6e3c6bce345185b3728a743f4db134
fazliddinKhayrullaev11 <fazliddinkhayrullaev9@gmail.com>
2023-02-25 11:43:51 +0500

    Ceil function

---
+++ JavaScript/Lesson-5/conditionals/index.js
@@ -0,0 +1,32 @@
+
+
+
+
+function statuss(number) {
+    if (number>0&&number<=10) {
+        console.log('Fail');
+        
+    } else if(number>10&&number<=20){
+        console.log('Passed');
+    }
+    else if(number>20&&number<=30){
+        console.log('Merit');
+    }
+    else if(number>30&&number<=35){
+        console.log('Distinction');
+    }
+    
+}
+
+ statuss(10) //Fail
+ statuss(11) //Passed
+ statuss(21)  //Merit
+ statuss(31)  //Distinction
+
+
+ function ceil(num) {
+     num>parseInt(num)&&console.log(parseInt(num)+1);
+     num==parseInt(num)&&console.log(num);
+ }
+
+ ceil(122.1)
