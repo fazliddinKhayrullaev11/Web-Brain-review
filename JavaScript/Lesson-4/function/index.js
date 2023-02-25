f0b7aaa79c61d119adf36cb5b813ea11edbe1310
fazliddinKhayrullaev11 <fazliddinkhayrullaev9@gmail.com>
2023-02-25 10:23:08 +0500

    default parametrs

--- JavaScript/Lesson-4/function/index.js
+++ JavaScript/Lesson-4/function/index.js
@@ -14,3 +14,24 @@ console.log(func());
  
 
  myFunction('Fazliddin' ,'+99899.948.37.79');
+ myFunction('Rayhkona' ,'+99894.618 .0970');
+
+
+ let str= 'IUGWlsjljqw89ey39y13';
+
+ console.log(str.match(/[0-9]/g));
+ console.log(str.match(/[a-z]/g));
+ console.log(str.match(/[A-Z]/g));
+
+
+
+ function person(name='Fazliddin' , phone='777777', age='25') { //default parametrs
+    console.log(`Hello ${name} !!!  Phone is :  ${phone}  age is : ${age}`);
+    
+ }
+
+person('Messi','55555' ,'35');
+person();
+
+
+
